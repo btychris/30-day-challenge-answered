5ae724ed5db489ac1bad302365c9d42e04ca384d
Chris Botty <chrisbotty13@gmail.com>
2025-09-04 21:53:43 +0100

    Create set level 3 challenge.py

---
+++ set level 3 challenge.py
@@ -0,0 +1,20 @@
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+st_age = set(age)
+
+print(age)
+print(len(age))
+
+print(st_age)
+print(len(st_age))
+
+# string = collection of characters
+# List = collection which are ordered and mutable
+# Tuple = collection which are ordered and immutable
+# Set = collection which are unordered and have no index, doesn't allow duplicates
+
+txt = "I am a teacher and I love to inspire and teach people."
+
+x = txt.split()
+
+st = set(x)
+print(st)
